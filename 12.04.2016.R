e5771b2f3e010c52a004112a20ff829e53a63250
ChristianaMcDonald-Spicer <cmcdonaldspicer@gmail.com>
2016-04-12 16:59:55 +1000

    change = to <-

--- 12.04.2016.R
+++ 12.04.2016.R
@@ -1,11 +1,11 @@
-compute = function(input_string){
+compute <- function(input_string){
   #do simple arithmetic from strings
   # Example: compute('5 + 2') -> 7
   #merged
-  values = unlist(strsplit(input_string, ' '))
-  num0 = as.integer(values[1])
-  operator =values[2]
-  num1 = as.integer(values[3])
+  values <- unlist(strsplit(input_string, ' '))
+  num0 <- as.integer(values[1])
+  operator <- values[2]
+  num1 <- as.integer(values[3])
  result <- switch(operator,
          '+' = num0 + num1,
          '-' = num0 - num1,
